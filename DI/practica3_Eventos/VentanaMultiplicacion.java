8dc3acf30d8f019216ebc95d59e4f5ad84c2ea53
Jaime Garcia <115151203+jgp14@users.noreply.github.com>
2022-11-04 09:56:57 +0100

    Add files via upload

--- DI/practica3_Eventos/VentanaMultiplicacion.java
+++ DI/practica3_Eventos/VentanaMultiplicacion.java
@@ -4,13 +4,6 @@ package practica3_Eventos;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.plaf.DimensionUIResource;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.net.Socket;
 
 //Creacion de ventanaMultiplicacion modelo
 public class VentanaMultiplicacion extends JFrame {
@@ -173,4 +166,5 @@ public class VentanaMultiplicacion extends JFrame {
         botonSuma.addActionListener(new ManejadorMultiplicacion(this));        
         numberField2.addKeyListener(new ManejadorMultiplicacion(this));
     }
+    
 }
