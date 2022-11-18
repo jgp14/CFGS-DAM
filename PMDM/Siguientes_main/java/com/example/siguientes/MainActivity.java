e7d2f6a78b98ed76ba00685988ab27e20dfaa497
Jaime Garcia <115151203+jgp14@users.noreply.github.com>
2022-11-18 09:21:23 +0100

    Add files via upload

---
+++ PMDM/Siguientes_main/java/com/example/siguientes/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.siguientes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i("EJEMPLO", "Estoy onStart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i("EJEMPLO", "Estoy onResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i("EJEMPLO", "Estoy onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i("EJEMPLO", "Estoy onStop");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.i("EJEMPLO", "Estoy onRestart");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("EJEMPLO", "Estoy onDestroy");
+    }
+
+}
