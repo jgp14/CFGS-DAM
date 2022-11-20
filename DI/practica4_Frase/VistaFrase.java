5da3cc1f305ec2666f702ae14c2a20b75648b584
Jaime Garcia <115151203+jgp14@users.noreply.github.com>
2022-11-20 01:54:10 +0100

    Delete DI directory

--- DI/practica4_Frase/VistaFrase.java
+++
@@ -1,9 +0,0 @@
-package practica4_Frase;
-
-public class VistaFrase {
-    
-    public static void main(String[] args){
-        ManejadorFrase manejadorFrase = new ManejadorFrase();
-        manejadorFrase.ventanaFrase.setVisible(true);
-    }
-}
