d0307bb17ebf895c93b3eb61aa8d5401c4caae88
Jaime Garcia <115151203+jgp14@users.noreply.github.com>
2022-11-21 23:31:04 +0100

    Add files via upload

---
+++ PSP/ud1_boletin8/Aleatorios.java
@@ -0,0 +1,42 @@
+package ud1_boletin8;
+
+import java.io.*;
+
+public class Aleatorios {
+
+	public static void main(String[] args) {
+		Runtime runTime = Runtime.getRuntime();
+		Process process = null;
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		String cadena = "";
+		//String rutaLin = "/media/user/KIOXIA/DAM/PSP/avaliacion1/src/ud2_boletin8/";
+		String rutaWin = "I:\\DAM\\PSP\\avaliacion1\\src\\ud1_boletin8\\";
+		String comando = "java "+rutaWin+"AleatoriosHijo.java";
+		
+		try {
+			System.out.println("Introduzca una linea");
+			cadena = br.readLine();
+			while(!cadena.equals("fin")) {
+				System.out.println("Sigue leyendo");
+				process = runTime.exec(comando);
+				BufferedReader in = new BufferedReader
+						(new InputStreamReader(process.getInputStream()));
+				System.out.println(in.readLine());
+				System.out.println("Introduzca una linea");
+				cadena = br.readLine();
+			}
+		}catch(IOException e) {
+			System.out.println("Error de entrada-salida");
+			System.exit(-1);
+		}
+		
+		try {
+			process.waitFor();
+		} catch(InterruptedException e) {
+			System.out.println("No pudimos esperar por termino");
+			System.exit(-1);
+		}
+	}
+
+}
---
+++ PSP/ud1_boletin8/AleatoriosHijo.java
@@ -0,0 +1,11 @@
+package ud1_boletin8;
+
+public class AleatoriosHijo {
+
+	public static void main(String[] args) {
+		int num;
+		num = ((int)(Math.random()*100) + 1);
+		System.out.println("Numero aleatorio: "+ num);
+	}
+
+}
